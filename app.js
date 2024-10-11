7c71061aeb1deddaec01ada9296264b06af1b64d
abudsistem <abudsistem@gmail.com>
2024-10-11 15:00:30 -0500

    creando notas

--- app.js
+++ app.js
@@ -7,6 +7,42 @@ const notesRouter = require('./controllers/notes')
 const usersRouter = require('./controllers/users')
 const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
+//const Note = require('../models/note')
+//const User = require('../models/user')
+
+const initialNotes = [
+  {
+    content: 'HTML is easy',
+    important: false
+  },
+  {
+    content: 'Browser can execute only JavaScript',
+    important: true
+  }
+]
+
+const nonExistingId = async () => {
+  const note = new Note({ content: 'willremovethissoon' })
+  await note.save()
+  await note.deleteOne()
+
+  return note._id.toString()
+}
+
+
+const usersInDb = async () => {
+  const users = await User.find({})
+  return users.map(user => user.toJSON())
+}
+
+const notesInDb = async () => {
+  const notes = await Note.find({})
+  return notes.map(note => note.toJSON())
+}
+
+module.exports = {
+  initialNotes, nonExistingId, notesInDb, usersInDb
+}
 const mongoose = require('mongoose')
 //const supertest= require('supertest')
 mongoose.set('strictQuery', false)
